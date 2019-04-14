daf7511fa5e2cbcbbaaac086b628f39467780a1a
unknown <selena.yu@ucalgary.ca>
2019-04-13 20:30:58 -0600

    User login/signup validation added

--- app.js
+++ app.js
@@ -4,8 +4,60 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/*----
+ROUTES
+-----*/
+//Main Page Route
+app.get("/", function(req, res){
+	res.render('index');
+});
+
+app.post('/login',function (req, res) {
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "tablefordays471",
+		database: "mysql"
+	});
+
+	con.connect(function(err) {
+		if (err) throw err;
+		let sql = "SELECT * FROM user_info WHERE username = ? AND password = ?";
+		let username = req.body.userID;
+		let password = req.body.userPassword;
+		con.query(sql, [username, password], function (err, result) {
+			if (err) throw err;
+			else res.send(result);
+		});
+	});
+});
+
+app.post('/createUser',function(req,res){
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "tablefordays471",
+		database: "mysql"
+	});
+
+	con.connect(function(err) {
+		let sql = "INSERT INTO user_info (email, username, password) VALUES (?,?,?)";
+		let email = req.body.userEmail;
+		let username = req.body.userID;
+		let password = req.body.userPassword;
+		con.query(sql, [email, username, password], function (err, result) {
+			if (err) throw err;
+			else res.send(result);
+		});
+	});
+});
 
 function onConnection(socket){
   socket.on('drawing', function(data){
